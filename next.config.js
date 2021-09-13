5b6b223b5703cd4eb164238a13e62db37afbfafc
snrntpa2z <snrntpa2z@naver.com>
2021-09-14 02:34:36 +0900

    fix: build error

--- next.config.js
+++ next.config.js
@@ -18,12 +18,5 @@ module.exports = {
     });
 
     return config;
-  },
-  publicRuntimeConfig: {
-    firebase: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-    },
-  },
+  }
 };
